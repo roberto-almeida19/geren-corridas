cbd50522f248710895c8015588d67f3032875344
Roberto <roberto-almeida19@outlook.com>
2019-07-18 01:22:27 -0300

    Adicionando Foto

--- src/components/itemLista.js
+++ src/components/itemLista.js
@@ -5,13 +5,19 @@ class ItemLista extends React.Component{
 
     render(){
         const items = this.props.item;
+        console.log(items.urlFoto);
+        let altFoto = `Foto do ${this.props.pessoa}`
+        altFoto = altFoto.slice(0,altFoto.length-1);
         return (
-            <div >
+            <div className='container-items'>
             {items.map(item =>(
                 <article key={item.id} className="item-lista">
-                <p>Id:{item.id}</p>
-                <p>Nome:{item.nome}</p>
-                <p>Endereco: {item.endereco.logradouro}, {item.endereco.numero}</p>
+                    <div className='img-foto'><img src={item.urlFoto} alt={altFoto}/></div>
+                    <div className='info-pessoa'>
+                        <p>Nome:{item.nome}</p>
+                        <p>Endereco: {item.endereco.logradouro}, {item.endereco.numero}</p>
+                        <p></p>
+                    </div>
                 </article>
             ))}
             </div>
--- src/components/lista.js
+++ src/components/lista.js
@@ -12,31 +12,32 @@ class Lista extends React.Component{
   carregarCliente = async () =>{
     const response = await api.get(`/clientes`);
     this.setState({lista: response.data});
+    
   };
   carregarMotoristas = async () =>{
     const response = await api.get(`/motoristas`);
     this.setState({lista: response.data});
   };
   verificaLista(){
-  
     if(this.props.clicked === 'Clientes')
       this.carregarCliente()
     else if (this.props.clicked === 'Motoristas') 
       this.carregarMotoristas()
   }
-  componentDidMount(){
+  componentDidUpdate(prevProps, prevState) {
     this.verificaLista();
   }
 
 
   render(){
+   // this.verificaLista();
     const item = this.state.lista;
-    console.log(item);
     
     
   return(
     <div>
-     <ItemLista item={item}/>
+ 
+     <ItemLista item={item} pessoa={this.props.clicked}/>
 
         
   </div>

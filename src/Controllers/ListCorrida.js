61c6447739e0f2b5485d96bd3cf830730c6ac2e9
Roberto <roberto-almeida19@outlook.com>
2019-07-18 00:14:28 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ src/Controllers/ListCorrida.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class ListaCorrida extends React.Component{
+
+
+
+
+
+
+    render(){
+        return;
+    }
+}
+export default ListaCorrida;
---
+++ src/Controllers/ListPessoa.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class ListaPessoa extends React.Component{
+
+
+
+
+
+
+    render(){
+        return;
+    }
+}
+export default ListaPessoa;
